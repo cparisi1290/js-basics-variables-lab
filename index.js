81899d128d97605f1e9711ad77e2306dd0364865
Cassandra <cparisi1290@gmail.com>
2021-03-18 10:04:49 -0400

    commit tree 92a3ebf3f202019e59cff15ef28c8f3b522931a3

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+const companyName = 'Scuber'; 
+// let companyName = 'Scuber'; 
+
+
+let mostProfitableNeighborhood = 'Chelsea'
+// const mostProfitableNeighborhood = 'Chelsea'
+
+// const companyCeo = 'Susan Smith'
+let companyCeo = 'Susan Smith'
